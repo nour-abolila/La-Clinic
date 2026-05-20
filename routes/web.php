4b1301976ccc260583bad52b7c53d29ddd093dda
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-20 22:53:26 +0300

    implement core e-commerce functionality including shopping cart, wishlist, product categories, and store views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\WishlistController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\User\AnimalsProductsController;
 use App\Http\Controllers\User\HomeProductsController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +14,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -20,6 +23,17 @@ Route::middleware('auth')->group(function () {
     // Review routes
     Route::get('/add-review', [ReviewController::class, 'create'])->name('reviews.create');
     Route::post('/add-review', [ReviewController::class, 'store'])->name('reviews.store');
+
+    // Wishlist routes
+    Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
+    Route::post('/wishlist/toggle/{product}', [WishlistController::class, 'toggle'])->name('wishlist.toggle');
+
+    // Cart routes
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+    Route::patch('/cart/update/{cart}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('/cart/remove/{cart}', [CartController::class, 'remove'])->name('cart.remove');
+    Route::get('/cart/data', [CartController::class, 'getCartData'])->name('cart.data');
 });
 
 
